813104d5a97e5fb00f5ddc110ecfc8626a51e792
Divyansh <divyanshgoel777@gmail.com>
2023-03-30 14:42:06 +0530

    Files Uploaded

--- redis/redis.js
+++ redis/redis.js
@@ -2,7 +2,7 @@ const redis= require("redis");
 require("dotenv").config();
 
 const client = redis.createClient({
-    url:`redis://default:${process.env.pass}@redis-19330.c212.ap-south-1-1.ec2.cloud.redislabs.com:19330`
+    url:process.env.redisURL
   })
   
     client.on("error", (err) => console.log(err, "ERROR in REDIS"));
